42e0b599e102f59ae7dbe200dfc6e17b288ea0b5
Adrian Sahlman <Tetratrio@users.noreply.github.com>
2020-08-20 05:30:01 +0200

    fix: use correct device argument for resuming training when loading model state dicts

--- stylegan2/train.py
+++ stylegan2/train.py
@@ -879,7 +879,7 @@ class Trainer:
         obj = cls(dataset=dataset, **loaded_kwargs)
         for name in ['G_opt', 'D_opt']:
             fpath = os.path.join(checkpoint_path, name + '.pth')
-            state_dict = torch.load(fpath, map_location=torch.device(loaded_kwargs['device']))
+            state_dict = torch.load(fpath, map_location=device)
             getattr(obj, name).load_state_dict(state_dict)
         return obj
 
